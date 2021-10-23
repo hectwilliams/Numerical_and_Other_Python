d9769dbca56d54f39cee94deacaf60c1a4d922bc
hectwilliams <hectwilliams@gmail.com>
2021-10-22 19:46:22 -0700

    heapfiy - quicksort and updated profiler

--- Inhertiance_Encapsulation_Polymorphism.py
+++ Inhertiance_Encapsulation_Polymorphism.py
@@ -1,12 +1,30 @@
 import random
 import numpy as np
 from typing import List
+from math import ceil
 
 import documentation_ as doc
 
-# !! Never forget
-# args(*) tuple inputs
-# args(**) key/value inputs
+'''
+
+ !! Never forget
+ args(*) tuple inputs
+ args(**) key/value inputs
+
+
+  @ Object has support for an operation, but not yet implemented -> NotImplementedError
+    def identity(data: object) -> Any:
+
+      raise NotImplementedError
+
+  @ Passing arguments of incorrect type -> TypeError
+    Operation on an object is not supported
+
+  @ Passing arguments with incorrect values -> ValueError
+    test(string, string, int)
+    expect(string, string,string)
+
+'''
 
 MESSAGE_LIST = [
   "Salutation",
@@ -63,7 +81,11 @@ class SubClass(SuperClass):
     sort_func_selector = [
       bubble_sort,
       insertion_sort,
-      selection_sort
+      selection_sort,
+      quick_sort,
+      merge_sort,
+      max_heap,
+      min_heap,
     ][option] # immediate execution
 
     if type(obj) is dict:
@@ -139,10 +161,103 @@ def insertion_sort (collection: List[int]) -> None:
         break
 
 def quick_sort (collection: List[int]) -> None:
-  # partition logic
+  '''
+    Recursion is replaced with table storing sub-arrray head and tail positions of nonpartitioned array slots
+  '''
+  table = []
+  pivot = partition_pos = None
+  sub_array_bound = None  # stores min max index of array e.g. [0 , 6]
+  swap_pos = 0 # position of leftmost available array slot where values <= pivot can be swapped(i.e. moved to left most side of partition). At the end of array iteration, swap_pos -1 holds the parition position where the array is split into two sub_arrays
+  tmp_data = None  # for swappable logic
 
-  #recursion
+  table.append([0, collection.__len__() - 1])   #init table
+  while table.__len__() !=0:
+    sub_array_bound = table.pop(0) # remove from head of array
+    pivot = collection[sub_array_bound[1]] # pivot located at end of sub_array tail
+    swap_pos = sub_array_bound [0] # lowest position is actually lowest sub-array bound
+    for i in range(sub_array_bound[0], sub_array_bound[1] + 1):
+      if collection[i] <= pivot:
+        # swap
+        tmp_data = collection[swap_pos]
+        collection[swap_pos] = collection[i]
+        collection[i] = tmp_data
+        # swap position not updated on tail index
+        if i != sub_array_bound[1]:
+          swap_pos = swap_pos + 1  # move swap position marker forward
+    # if left side exists, add min and max indices to table (refer to sub_array_bound )
+    if swap_pos - 1 > sub_array_bound[0]:
+      table.append([sub_array_bound[0], swap_pos - 1])
+    # if right side exists, add to table
+    if swap_pos + 1 < sub_array_bound[1]:
+      table.append([swap_pos + 1 , sub_array_bound[1]])
 
-  pass
+def merge_sort (collection: List[int]) -> None:
+  array_a = array_b = None
+  mid = int(collection.__len__() / 2)
+  pos_a = pos_b = None
+  if mid == 0:
+    return
+  else:
+    array_a = collection[0 : mid]
+    array_b = collection[mid : :]
+    merge_sort(array_a)
+    merge_sort(array_b)
+    pos_a = pos_b = 0
+    for i in range(collection.__len__()):
+      # overwrite input array with array b and a elements (added in order from least to greatest)
+      if pos_b == array_b.__len__() :
+        collection[i] = array_a[pos_a]
+        pos_a += 1
+      elif pos_a == array_a.__len__() :
+        collection[i] = array_b[pos_b]
+        pos_b += 1
+      elif array_a[pos_a] <= array_b[pos_b]   :
+        collection[i] = array_a[pos_a]
+        pos_a += 1
+      elif array_b[pos_b] <= array_a[pos_a] :
+        collection[i] = array_b[pos_b]
+        pos_b += 1
+
+def swap(collection: List, n: int, m: int ):
+  tmp = collection[n]
+  collection[n] = collection[m]
+  collection[m] = tmp
+
+def heapfiy(collection: List[int] , block_count = 0, is_max_heapify: bool = True) -> None:
+  current_node_pos = min_node_pos = ceil( collection.__len__() / 2 )
+  child_node_left_pos = child_node_right_pos = None
+
+  while (current_node_pos >=0) :
+    # set min node to current node
+    min_node_pos = current_node_pos
+    #check left node for min
+    child_node_left_pos  = 2 * current_node_pos + 1
+    if child_node_left_pos < collection.__len__() - block_count:
+      if collection[child_node_left_pos] < collection[min_node_pos] if is_max_heapify else  collection[child_node_left_pos] > collection[min_node_pos] :
+        min_node_pos = child_node_left_pos
+    #check right node for min
+    child_node_right_pos  = 2 * current_node_pos + 2
+    if child_node_right_pos < collection.__len__() - block_count:
+      if collection[child_node_right_pos] < collection[min_node_pos] if is_max_heapify else collection[child_node_right_pos] > collection[min_node_pos] :
+        min_node_pos = child_node_right_pos
+    #swap if current_node(node position upon enternig loop)  does not match min_node
+    if min_node_pos != current_node_pos:
+      swap(collection, min_node_pos, current_node_pos)
+    current_node_pos -= 1
 
 
+def max_heap(collection: List):
+
+  for i in range(collection.__len__() - 1):
+    #heap
+    heapfiy(collection, i)
+    #swap zeroth element with next available array slot in tail (i.e. array_length - 1 - i)
+    swap(collection, 0, collection.__len__() - 1 - i)
+
+def min_heap(collection: List):
+
+  for i in range(collection.__len__() - 1):
+    #heap
+    heapfiy(collection, i, False)
+    #swap zeroth element with next available array slot in tail (i.e. array_length - 1 - i)
+    swap(collection, 0, collection.__len__() - 1 - i)
--- profiler.py
+++ profiler.py
@@ -4,21 +4,6 @@ from typing import Any
 
 from Inhertiance_Encapsulation_Polymorphism import SubClass
 
-'''
-
-  @ Object has support for an operation, but not yet implemented -> NotImplementedError
-    def identity(data: object) -> Any:
-
-      raise NotImplementedError
-
-  @ Passing arguments of incorrect type -> TypeError
-    Operation on an object is not supported
-
-  @ Passing arguments with incorrect values -> ValueError
-    test(string, string, int)
-    expect(string, string,string)
-'''
-
 __all__ = [
   "Profiler"
 ]
@@ -40,18 +25,26 @@ class Profiler():
     time_array.append( datetime.datetime.now().microsecond )
     input_args_str = str(args)
     obj = {} # for 'other' data from tests within test chain
-
+    hide_data = False
     for i in range(n):
       time_array.append( datetime.datetime.now().microsecond )
       result = str(functor(*args, obj)) # call
       time_array[-1] = datetime.datetime.now().microsecond - time_array[-1]
-      print('\nFunctionName = {0}\nInput(i.e. args) = {1}\nResult = {2}\nTimeElapsed = {3} ms \n'.format( str(obj.get('name')) , input_args_str , str(result), time_array[-1] / 10e3 ))
+      print('{4}\nFunctionName = {0} \nTimeElapsed = {3} ms\nInput(i.e. args) = {1}\n Result = {2} \n{4}'.format( str(obj.get('name')) , 'hidden' if hide_data else  input_args_str   ,'hidden' if hide_data else str(result), time_array[-1] / 10e3 , ("--"*100) + '\n' ))
     time_array[0] = datetime.datetime.now().microsecond - time_array[0]
 
   def test(self, N = 50):
+    test_data = random.sample(range(0,10000), 10000)
     # self.test_gen(N, sum, [10, 1, 11])
-    self.test_gen(1,SubClass().sort_number_list, random.sample(range(0,1000), 1000), 2)
-    self.test_gen(1,SubClass().sort_number_list, random.sample(range(0,1000), 1000), 1)
-    self.test_gen(1,SubClass().sort_number_list, [10,80,30,90,40,50,70], 3)
+    # self.test_gen(1,SubClass().sort_number_list, random.sample(range(0,1000), 1000), 3)
+    # self.test_gen(1,SubClass().sort_number_list, test_data[: :] , 1)
+    # self.test_gen(1,SubClass().sort_number_list, test_data[: :] , 2)
+    # self.test_gen(1,SubClass().sort_number_list, test_data[: :], 3)
+    # self.test_gen(1,SubClass().sort_number_list, [test_data[: :]], 4)
+    # self.test_gen(1,SubClass().sort_number_list, [test_data[: :]], 5)
+    self.test_gen(1,SubClass().sort_number_list, [7, 3, 2, 16, 24, 4, 11, 9], 5)
+    self.test_gen(1,SubClass().sort_number_list, [7, 3, 2, 16, 24, 4, 11, 9], 6)
+
+
 
 Profiler().test()
